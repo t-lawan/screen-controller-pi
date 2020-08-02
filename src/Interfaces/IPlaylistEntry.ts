e81fa1c4ef10486cf2a1a1faf219e5a95269c1ac
akinsola <thomas.lawanson@gmail.com>
2020-08-02 10:57:06 +0100

    update

---
+++ src/Interfaces/IPlaylistEntry.ts
@@ -0,0 +1,4 @@
+export interface IPlaylistEntry {
+    id: string;
+    order: number;
+}
--- src/Interfaces/IScreen.ts
+++ src/Interfaces/IScreen.ts
@@ -1,9 +1,10 @@
 import { EScreenType } from '../Enums/EScreenType';
+import { IPlaylistEntry } from './IPlaylistEntry';
 export interface IScreen {
     id?: string; 
     local_ip_address: string;
     raspberry_pi_id: number;
     number_of_screens: number;
-    video_file_playlist: string[];
+    video_file_playlist: IPlaylistEntry[];
     screen_type: EScreenType;
 }
